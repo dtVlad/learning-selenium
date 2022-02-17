9e902a23347ea64978fd08bce340dd5849e33303
Volodymyr Korostelov <volodymyrkorostelov@gmail.com>
2022-02-17 13:12:51 +0200

    First Task in Section 2
    First task about selenium infrastructure in section 2

---
+++ ST_Homework/Section2Task/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Section2Task")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Section2Task")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3ac56e9d-f566-4489-86ce-e3d66f94c414")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ ST_Homework/Section2Task/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using System;
+using OpenQA.Selenium.Chrome;
+using SeleniumExtras.WaitHelpers;
+
+namespace Section2Task
+{
+    public class UnitTest1
+    {
+        [TestFixture]
+        public class MyFirstTest
+        {
+            private IWebDriver driver;
+            private WebDriverWait wait;
+
+            [SetUp]
+            public void start()
+            {
+                driver = new ChromeDriver();
+                wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+            }
+
+            [Test]
+            public void FirstTest()
+            {
+                driver.Url = "https://www.wikipedia.org/";
+                driver.Manage().Window.Maximize();
+                driver.FindElement(By.Name("search")).SendKeys("program");
+                driver.FindElement(By.CssSelector("[type='submit']")).Click();
+                wait.Until(ExpectedConditions.TitleContains("Program"));
+                
+            }
+
+            [TearDown]
+            public void stop()
+            {
+                driver.Quit();
+                driver = null;
+            }
+        }
+    }
+}
